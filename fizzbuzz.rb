5a153736cd8943bef70b3992da8839740db866ad
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-08-08 16:20:37 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,13 @@
 
 
 def fizzbuzz(int)
+<<<<<<< HEAD
 if int % 5 == 0 && int % 3 == 0
 return "FizzBuzz"
+=======
+if int % 5 == 0 && if int % 3 == 0
+  return "FizzBuzz"
+>>>>>>> f49a5e100ebe122678740e63f2c99f389cbfc723
 
 elsif int % 5 == 0
 return "Buzz"
@@ -10,8 +15,13 @@ return "Buzz"
 elsif int % 3 == 0
   return "Fizz"
   
+<<<<<<< HEAD
 else int % 5 != 0 && int % 3 != 0
   return nil
+=======
+else
+  return "nil"
+>>>>>>> f49a5e100ebe122678740e63f2c99f389cbfc723
 end
 end
 
